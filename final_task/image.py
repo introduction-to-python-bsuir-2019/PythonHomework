de42386e8a57dc52cafe08605f6f8f7f1f496cdc
Polina Pashkovskaya <polly.pashkovskaya@gmail.com>
2019-10-29 17:41:40 +0300

    add rss-reader base functionality

---
+++ final_task/image.py
@@ -0,0 +1,8 @@
+class Image:
+    """
+    This class represents information about image like link and alternative text
+    """
+
+    def __init__(self, link, alt):
+        self.link = link
+        self.alt = alt
