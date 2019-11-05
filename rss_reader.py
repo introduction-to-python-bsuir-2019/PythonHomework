65f21db5e4edde2cdd2389018308e10ad2d73ef4
Aliaksei Dzemasiuk <mr.glazik@gmail.com>
2019-11-05 15:17:05 +0300

    Help page fully operational

--- rss_reader.py
+++ rss_reader.py
@@ -1,15 +1,13 @@
 import argparse
 
-parser=argparse.ArgumentParser(description='HIT ME MAN')
-parser.add_argument('-v',action='store_true',help='show this help message and exit')
-#parser.add_argument('--version',action='store_true',help='Print version info')
-#parser.add_argument('--json',action='store_true',help='Print result as JSON in stdout')
+parser=argparse.ArgumentParser(description='Pure Python command-line RSS reader.')
+parser.add_argument('source',help='RSS URL')
+parser.add_argument('--version',action='store_true',help='Print version info')
+parser.add_argument('--json',action='store_true', help='Print result as JSON in stdout')
+parser.add_argument('--verbose',action='store_true',help='Outputs verbose status messages')
+parser.add_argument('--limit', type=int,action='store',help='Limit news topics if this parameter provided')
+
+print(parser)
 
 
 args=parser.parse_args()
-if args.v:
-    print('v')
-#if args.version:
-#    print('version')
-#if args.json:
-#    print('json')
