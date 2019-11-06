e47312d59ebf870d1cacdfa99f1a64c584ccd257
Aliaksei Dzemasiuk <mr.glazik@gmail.com>
2019-11-06 11:17:16 +0300

    Argument checks implemented

--- rss_reader.py
+++ rss_reader.py
@@ -1,5 +1,6 @@
 import argparse
-from functools import reduce
+import requests
+from bs4 import BeautifulSoup
 
 parser=argparse.ArgumentParser(description='Pure Python command-line RSS reader.')
 parser.add_argument('source',nargs='?',help='RSS URL')
@@ -9,9 +10,10 @@ parser.add_argument('--verbose',action='store_true',help='Outputs verbose status
 parser.add_argument('--limit', type=int,action='store',help='Limit news topics if this parameter provided')
 args=parser.parse_args()
 
-version='v1.0.1'
-err={1: 'Incorrect argument input. Either --version or link are allowed',
-    2: 'Incorrect link',}
+version='v1.0'
+err={1: 'You don\'t use --version together with other arguments',
+    2: 'Source or --version expected',
+    3: 'Entered arguments require source as well'}
 
 
 class Error(Exception):
@@ -27,16 +29,24 @@ def verboser(func,action):
         return result
     return wrapper
 
+def retrieve_news(*args):
+    pass
 
-#if args.version==reduce(lambda x,y: x or y,list(map(bool,[args.limit,args.source,args.json,args.verbose]))):
-#    raise Error(err[1)
+def make_json(*args):
+    pass
 
 
+if args.version and (args.source or args.json or args.verbose or args.limit):
+    raise Error(err[1])
+if not (args.version or args.source):
+    raise Error(err[2])
 
-#if args.version:
-#    print('Current version of RSS-reader: '+version)
-#
-#print(args)
-#TODO: --limiting
-#TODO: --json (limiting included)
-#TODO: --verbose
+if args.version:
+    print('Current version of RSS-reader: '+version)
+else:
+    if args.verbose:
+        retrieve_news=verboser(retrieve_news,'retrieving')
+        make_json=verboser(make_json,'making JSON')
+    news=retrieve_news(args.source,args.limit)
+    if args.json:
+        make_json(news)
