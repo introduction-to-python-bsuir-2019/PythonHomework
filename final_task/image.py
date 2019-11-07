0d583b549dacbbdb866a33d4bbc33fc78dd8a02a
Pashkovskaya Polina <polly.pashkovskaya@gmail.com>
2019-11-07 13:13:55 +0300

    Delete image.py

--- final_task/image.py
+++
@@ -1,8 +0,0 @@
-class Image:
-    """
-    This class represents information about image like link and alternative text
-    """
-
-    def __init__(self, link, alt):
-        self.link = link
-        self.alt = alt
