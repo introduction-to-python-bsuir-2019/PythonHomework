9817212f8d84c42bc1ba8be6ff90e5f47f77aee8
elaskin <mr.elaskin@mail.ru>
2019-11-07 18:30:24 +0300

    add logging

--- RSS-Reader/main.py
+++ RSS-Reader/main.py
@@ -1,10 +1,20 @@
+'''Main module of RssReader which starts program.'''
+
+import logging
+from logging import config
 import argparse
-# import feedparser
 
 from rss_parser import RssReader
 
 
-VERSION = 'version 1.0'
+VERSION = '1.0'
+
+LOGS_FILENAME = "rss_reader.log"
+
+ROOT_LOGGER_NAME = 'RssReader'
+MODULE_LOGGER_NAME = ROOT_LOGGER_NAME + '.main'
+
+CORRECT_END_LOG = 'END (correct)'
 
 # LINK = 'https://www.reddit.com/.rss'
 
@@ -12,49 +22,94 @@ LINK = 'https://news.yahoo.com/rss/'
 # LINK = 'https://www.newsisfree.com/rss/'
 
 
-def init_args(parser):
+def create_args_parser() -> argparse.ArgumentParser:
+    '''Func which creats parser of arguments of command-line.
+        Return: argparse.ArgumentParser
+
+    '''
+    logger = logging.getLogger(MODULE_LOGGER_NAME + '.create_args_parser')
+    logger.info("Create parser of arguments")
+
+    parser = argparse.ArgumentParser(description='Command-line RSS reader.')
+    return parser
+
+
+def init_args(parser: argparse.ArgumentParser) -> None:
+    '''Func which takes parser and adds arguments.
+       -v, --version
+       --json
+       -l, --limit
+       link
+       -V, --verbose 
+    '''
+    logger = logging.getLogger(MODULE_LOGGER_NAME + '.init_args')
+    logger.info('Initialize arguments of command-line')
+
     parser.add_argument(
+        '-v',
         '--version',
         help='Print version info',
         action='version',
-        version=VERSION
+        version=('version ' + VERSION)
     )
-
     parser.add_argument(
         '--json',
         help='Print result as JSON in stdout',
         action='store_true'
     )
-
     parser.add_argument(
+        "-l",
         '--limit',
         type=int,
         help='Limit news topics if this parameter provided'
     )
-
     parser.add_argument(
         "link",
         type=str,
-        metavar='LINK',
+        nargs='?',
         help='Link on RSS resource'
+    )
+    parser.add_argument(
+        "-V",
+        "--verbose",
+        help='Print all logs in stdout',
+        action='store_true'
     )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Command-line RSS reader.')
+    logging.config.fileConfig('logger_config.conf')
+    logger = logging.getLogger(MODULE_LOGGER_NAME)
+    logger.info('START')
+
+    parser = create_args_parser()
     init_args(parser)
     args = parser.parse_args()
 
-    rss_reader = RssReader(link=LINK)
+    if args.verbose is True:
+        logger.info('Output logs')
 
-    if args.json is True:
-        with open('news.json', 'w+') as file:
-            file.write(rss_reader.get_news_as_json())
+        with open(LOGS_FILENAME, 'r') as file:
+            print(file.read())
+
+        logger.info(CORRECT_END_LOG)
     elif args.link is not None:
+        logger.info('Entrance to get news case')
+
+        rss_reader = RssReader(link=LINK)
+
         if args.limit is not None:
             limit = args.limit
         else:
             limit = 0
 
-        news = rss_reader.get_news_as_string(limit=limit)    
+        if args.json is True:
+            news = rss_reader.get_news_as_json(limit=limit)
+        else:
+            news = rss_reader.get_news_as_string(limit=limit)
+
         print(news)
+        logger.info(CORRECT_END_LOG)
+    else:
+        logger.error('END (no args)')
+        parser.parse_args(['-h'])
--- RSS-Reader/rss_parser.py
+++ RSS-Reader/rss_parser.py
@@ -6,8 +6,9 @@ Inside are used modules:
 	- bs4 (BeautifulSoup)
 '''
 
-import feedparser
+from feedparser import parse, FeedParserDict
 from bs4 import BeautifulSoup
+import logging
 
 import json
 
@@ -16,54 +17,79 @@ NEWS_SEPARATOR = '=========================================================='
 EN = '\n' # enter
 DEN = '\n\n' # double enter
 
+ROOT_LOGGER_NAME = 'RssReader'
+MODULE_LOGGER_NAME = ROOT_LOGGER_NAME + '.rss_reader'
+
 
 class RssReader:
 	'''This class uses interface of feedparser, which interact with RSS-sources.
 	'''
 
-	def __init__(self, link):
+	CLASS_LOGGER_NAME = MODULE_LOGGER_NAME + '.RssReader'
+
+	def __init__(self, link: str):
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '.__init__')
+		logger.info(f'Creating object of RssReader class with link: {link}')
+
 		self.link = link
 
 
-	def _get_feed(self):
+	def _get_feed(self) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_feed')
+		logger.info('Getting title of resource')
+
 		return 'Feed: ' + self.rss.feed.title
 
 
-	def _get_title(self, new):
-		return 'Title: ' + new.title
+	def _get_title(self, one_news: FeedParserDict) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_title')
+		logger.info('Getting title of one news')
+
+		return 'Title: ' + one_news.title
 
 
-	def _get_date(self, new):
-		return 'Date: ' + new.published
+	def _get_date(self, one_news: FeedParserDict) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_date')
+		logger.info('Getting publishing date of one news')
+
+		return 'Date: ' + one_news.published
 
 
-	def _get_link(self, new):
-		return 'Link: ' + new.link
+	def _get_link(self, one_news: FeedParserDict) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_link')
+		logger.info('Getting link on one news')
+
+		return 'Link: ' + one_news.link
 
 
-	def _get_content(self, new):
-		return self._parse_elem(new.summary_detail.value)
+	def _get_content(self, one_news: FeedParserDict) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_content')
+		logger.info('Getting short content of one news')
+
+		return self._parse_elem(one_news.summary_detail.value)
 
 
-	def set_link(self, link):
-		self.link = link
+	def _get_rss(self) -> None:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_rss')
+		logger.info('Parse page with rss-format')
+
+		self.rss = parse(self.link)
 
 
-	def _get_rss(self):
-		self.rss = feedparser.parse(self.link)
+	def get_news_as_string(self, limit: int=0) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '.get_news_as_string')
+		logger.info(f'Getting news with string-format with limit: {limit}')
 
-
-	def get_news_as_string(self, limit=0):
 		self._get_rss()
 
 		feed = self._get_feed()
 		
 		news = ''
-		for new in self.rss.entries:
-			title = self._get_title(new)
-			date = self._get_date(new)
-			link = self._get_link(new)
-			content = self._get_content(new)
+		for one_news in self.rss.entries:
+			title = self._get_title(one_news)
+			date = self._get_date(one_news)
+			link = self._get_link(one_news)
+			content = self._get_content(one_news)
 
 			news += title + EN + date + EN + link + DEN + content + DEN
 			news += NEWS_SEPARATOR + DEN
@@ -75,12 +101,19 @@ class RssReader:
 		return feed + DEN + news
 
 
-	def get_news_as_json(self, limit=0):
+	def get_news_as_json(self, limit: int=0) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._get_news_as_json')
+		logger.info(f'Getting news with json-format \
+					with limit: {limit}')		
+
 		self._get_rss()
 		return json.dumps(self.rss, indent=4)
 
 
-	def _parse_elem(self, elem):
+	def _parse_elem(self, elem: str) -> str:
+		logger = logging.getLogger(self.CLASS_LOGGER_NAME + '._parse_elem')
+		logger.info('Extract short content of one news on html-format')
+
 		soup = BeautifulSoup(elem, "html.parser")
 		return soup.get_text()
 
