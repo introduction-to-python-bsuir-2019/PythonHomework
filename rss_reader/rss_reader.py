26ed2d31810681b14038bef1e5ae6dc700dad677
Borya <borya.dashko@gmail.com>
2019-11-09 00:52:56 +0300

    setup.py was added

---
+++ rss_reader/rss_reader.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='T.')
+    parser
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='rss_reader',
+    version='1.0',
+    author='Boris Dashko',
+    author_email='borya.dashko@gmail.com',
+    url='https://github.com/BoryaD/PythonHomework/tree/FinalTask',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
