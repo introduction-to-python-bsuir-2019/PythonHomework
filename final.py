b6f1944dd419992ae6a1e90992c290d3000e5cff
Ivan Denisenko <57327265+IvanDenisenko@users.noreply.github.com>
2019-11-10 21:48:19 +0200

    Add files via upload

---
+++ final.py
@@ -0,0 +1,12 @@
+import feedparser
+NewsFeed = feedparser.parse("https://auto.onliner.by/feed")
+entry1 = NewsFeed.entries[0]
+print(entry1.items)
+print ('Number of RSS posts :', len(NewsFeed.entries))
+
+for entry in NewsFeed.entries:
+
+	print (entry.title)
+	print (entry.published)
+
+	print ("******")
