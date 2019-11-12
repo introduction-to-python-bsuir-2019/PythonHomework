aec5dd76acf17224eb99ebe787a45da0bcc416c9
KirylDv <dkirill111@gmail.com>
2019-11-12 19:38:15 +0300

    Add: arguments for parser

--- rss_reader.py
+++ rss_reader.py
@@ -1,5 +1,14 @@
+import argparse
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source",    type=str,            help="RSS URL")
+    parser.add_argument("--version", action="store_true", help="Print version info")
+    parser.add_argument("--json",    action="store_true", help="Print result as JSON in stdout")
+    parser.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
+    parser.add_argument("--limit",   action="store_true", help="Limit news topics if this parameter provided")
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == "__main__":
     main()
