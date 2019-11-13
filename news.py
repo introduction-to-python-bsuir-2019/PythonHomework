2fb6715276bd557b41f5714a4b0b0dbb8f3445eb
Yana Shcherbich <vilikdf@gmail.com>
2019-11-13 22:14:07 +0300

    Add ability to convert into JSON and work with command line

---
+++ news.py
@@ -0,0 +1,170 @@
+"""Module contains class related to news"""
+
+import json
+import logging
+import sys
+
+import feedparser
+from bs4 import BeautifulSoup
+
+
+class RssReader():
+    """This class parse, process and output news."""
+
+    def __init__(self, url: str, limit=None):
+        logging.info('Initialization')
+
+        self.url = url
+        self.feeds = feedparser.parse(url)
+        self._check_url()
+
+        self.feed_title = self.feeds.feed.get('title')
+        self.list_of_news = []
+        self.limit = limit
+
+        self._check_limit()
+        self.make_list_of_news()
+
+    def _check_url(self):
+        """Check if the url is valid."""
+
+        logging.info('Check URL')
+        if self.feeds['bozo'] or self.feeds.status != 200:
+            logging.error('Something wrong with URL or Internet connection')
+            sys.exit(1)
+
+    def _check_limit(self):
+        """Check if the limit >= 0."""
+
+        logging.info('Check limit')
+        if self.limit is not None and self.limit < 0:
+            logging.error('Limit < 0')
+            sys.exit(1)
+
+    def print_news(self):
+        """Print news in human-readable format."""
+
+        logging.info('Print news')
+
+        print('Feed:', self.feed_title, "\n\n")
+
+        news_number = 1
+        for news in self.list_of_news:
+            print('№', news_number)
+            news_number += 1
+            print('Title:', news['title'])
+            print('Date:', news['date'])
+            print('Link:', news['link'], '\n')
+
+            if news['description']['text']:
+                print(news['description']['text'], '\n')
+
+            if news['description']['images']:
+                print('Images:')
+                for item in news['description']['images']:
+                    print(item)
+
+            if news['description']['links']:
+                print('Links:')
+                for item in news['description']['links']:
+                    print(item)
+
+            print('-' * 50)
+
+    def _find_date_tag(self, news: dict) -> str:
+        """
+        Find date tag and return its value,
+        or return 'Unknown' if tag not found.
+        """
+
+        logging.info('Find date tag')
+
+        if news.get('published'):
+            return news['published']
+        elif news.get('pubDate'):
+            return news['pubDate']
+        elif news.get('Date:'):
+            return news['Date']
+        else:
+            return 'Unknown'
+
+    def make_list_of_news(self):
+        """Make a list of news.
+
+        type of news: dict
+         """
+
+        logging.info('Make a list of news')
+
+        if self.limit is None or self.limit > len(self.feeds):
+            self.limit = len(self.feeds)
+
+
+        for news in self.feeds['entries'][:self.limit]:
+            one_news = {}
+
+            if news.get('title'):
+                one_news['title'] = news['title']
+            else:
+                one_news['title'] = 'Unknown'
+
+            if news.get('link'):
+                one_news['link'] = news['link']
+            else:
+                one_news['link'] = 'Unknown'
+
+            one_news['date'] = self._find_date_tag(news)
+            one_news.update(self._read_description(news))
+            self.list_of_news.append(one_news)
+
+    def _read_description(self, news: dict) -> dict:
+        """Return dict with keys 'text', 'images', 'links'.
+
+        'text' value is description(str)
+        'images' value is a list of images sources
+        'links' value is a list of urls
+        """
+
+        logging.info('Get information from description')
+        soup = BeautifulSoup(news.description, features="html.parser")
+
+        logging.info('Get text of description')
+        text = soup.text
+        if not text:
+            text = 'Nothing'
+
+        logging.info('Get list of images')
+        list_of_images = []
+        images = soup.findAll('img')
+        for image in images:
+            if image.get('src'):
+                list_of_images.append(image['src'])
+
+        if not list_of_images:
+            list_of_images = None
+
+        logging.info('Get list of links')
+        list_of_links = []
+        for tag in soup.findAll():
+            if tag.get('href'):
+                list_of_links.append(tag['href'])
+            if tag.get('url'):
+                list_of_links.append(tag['url'])
+
+        if not list_of_links:
+            list_of_links = None
+
+        return {'description': {'text': text, 'images': list_of_images,
+                'links': list_of_links}}
+
+    def convert_to_json(self):
+        """Return news in JSON format."""
+
+        logging.info('Convert news into JSON format')
+        try:
+            result = json.dumps({'news': {'feed': self.feed_title, 'items': self.list_of_news}},
+                                indent=4, ensure_ascii=False)
+        except Exception as e:
+            logging.error("Can't convert to JSON:", e)
+
+        return result
--- rss_reader.py
+++ rss_reader.py
@@ -1,97 +1,61 @@
-import feedparser
-from bs4 import BeautifulSoup
+"""Module provides work with command line"""
+
+import argparse
+import logging
+import sys
+
+from news import RssReader
+
+VERSION = "1.0"
 
 
-class RssReader():
+def add_args(parser) -> parser:
+    """Add arguments and return new parser."""
 
-    def __init__(self, url, limit=None):
-        self.url = url
-        self.feeds = feedparser.parse(url)
-        self.tags = ['title', 'published', 'pubDate', 'link', 'date']
-        self.list_of_news = []
-        self.limit = limit
+    parser.add_argument('source', help='RSS URL', type=str)
+    parser.add_argument('--version', help='Print version info', action='version')
+    parser.add_argument('--json', help='Print result as JSON in stdout', action="store_true")
+    parser.add_argument('--verbose', help='Outputs verbose status messages', action="store_true")
+    parser.add_argument('--limit', help='Limit news topics if this parameter provided', type=int)
+    return parser
 
+def start_parsing(url: str, limit: int, json_mode: bool):
+    """This function create rss feed and print news.
 
-    def print_date(self, news):
-        """print date"""
-        if news.get('published') != 'Unknown':
-            print('Date:', news['published'])
-        elif news.get('pubDate') != 'Unknown':
-            print('Date:', news['pubDate'])
-        elif news.get('date') != 'Unknown':
-            print('Date:', news['date'])
-        else print('Date: unknown')
+    Arguments:
+        url - RSS URL
+        limit - news amount that will be printed
+        json_mode - if true then news will be printed in JSON format
+    """
 
+    logging.info('Create feed')
+    feed = RssReader(url, limit)
 
-    def print_news(self):
-        """print news"""
-        self.make_list_of_news()
-        print('Feed:', self.feeds.feed.get('title'), "\n\n")
+    if json_mode:
+        print(feed.convert_to_json())
+    else:
+        feed.print_news()
 
-        for news in self.list_of_news:
-            print('Title:', news['title'])
-            self.print_date(news)
-            print('Link:', news['link'], '\n')
+def main():
+    """This function works with arguments, starts parsing."""
 
-            if news.get('text'):
-                print(news['text'], '\n')
+    parser = argparse.ArgumentParser(description='Pure Python command-line RSS reader')
+    parser = add_args(parser)
+    parser.version = VERSION
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
-            if news.get('images'):
-                print('Images:')
-                for link in news['images']:
-                    print(link)
-                print()
+    try:
+        start_parsing(args.source, args.limit, args.json)
+    except Exception as e:
+        logging.ERROR("Something wrong with parsing: {e}")
 
-            if news.get('links'):
-                print('Links:')
-                for link in news['links']:
-                    print(link)
-                print()
+    logging.info('Program is completed')
 
-            print('-' * 50)
-
-
-    def make_list_of_news(self):
-        """Make a list of news
-
-        type of news: dict
-         """
-
-        if self.limit == None or self.limit > len(self.feeds):
-            self.limit = len(self.feeds)
-
-        for news in self.feeds['entries'][:self.limit]:
-            one_news = {}
-            for tag in self.tags:
-                if tag in news:
-                    one_news[tag] = news[tag]
-                else:
-                    one_news[tag] = 'Unknown'
-            one_news.update(self.read_description(news))
-            self.list_of_news.append(one_news)
-
-
-    def read_description(self, news)->dict:
-        """Return dict with keys 'text', 'images', 'links'
-
-        'text' value is description(str)
-        'images' value is a list of images sources
-        'links' value is a list of urls
-
-        """
-        soup = BeautifulSoup(news.description, features="html.parser")
-
-        list_of_images = []
-        images = soup.findAll('img')
-        for image in images:
-            if image.get('src'):
-                list_of_images.append(image['src'])
-
-        list_of_links = []
-        for tag in soup.findAll():
-            if tag.get('href'):
-                list_of_links.append(tag['href'])
-            if tag.get('url'):
-                list_of_links.append(tag['url'])
-
-        return {'text': soup.text,'images': list_of_images, 'links': list_of_links}
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print('Something went wrong: ', e)
+        sys.exit(1)
