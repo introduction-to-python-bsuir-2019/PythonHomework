4617b7dd74d92f9e984f0b1c75a97cbc33191361
KirylDv <dkirill111@gmail.com>
2019-11-16 21:18:57 +0300

    Add: --json

--- reader.py
+++ reader.py
@@ -10,13 +10,29 @@ def output(string, sep=' ', end='\n', flush=False, verbose=True):
     if verbose:
         string = string.replace("&#39;", "'")
         print(string, sep=sep, end=end, flush=flush)
-    
+
+
+def progress(elems, done, length=20):
+    """Take arguments 
+    elems: count of elements
+    done: progress (in elements)
+    length: progress bar length
+    Write progress bar to stdout
+    """
+    if done != 0:
+        print("\r", end="")
+    col = int(length * (done/elems))
+    print(f"[{'='*col + ' '*(length-col)}] {int(100*done/elems)}%", end="")
+    if elems == done:
+        print()
+
 
 class RSSReader():
     """RSSReader: Class for reading rss channels.
     Methods:
     show_news() - output news to stdout
     """
+
     def __init__(self, args):
         super(RSSReader, self).__init__()
         self.__source = args.source
@@ -42,7 +58,6 @@ class RSSReader():
                 output("Unknown error")
             sys.exit()
 
-
     def __parse(self):
         """Parse XML data to python structures"""
         output("Parsing information...", verbose=self.__verbose)
@@ -50,6 +65,8 @@ class RSSReader():
         feed = xml.getElementsByTagName("title")[0].firstChild.data
         items = xml.getElementsByTagName("item")
         counter = 0
+        if self.__verbose:
+            progress(self.__limit, counter)
         column = []
         for item in items:
             if counter == self.__limit:
@@ -62,8 +79,8 @@ class RSSReader():
                         item.getElementsByTagName("link")[0].firstChild.data,
                         text,
                         links]]
-            output(f"{counter} article parsed.", verbose=self.__verbose)
-        output("Complete.", verbose=self.__verbose)
+            if self.__verbose:
+                progress(self.__limit, counter)
         return feed, column
 
     def show_news(self):
@@ -81,3 +98,32 @@ class RSSReader():
             for i in range(len(news[4])):
                 output(f"[{i+1}]: {news[4][i]}")
             output("\n\n")
+
+    def show_json(self):
+        """Read, parse, convert into json and print info in stdout"""
+        self.__read_news()
+        feed, column = self.__parse()
+        output("Convert to json...", verbose=self.__verbose)
+        counter = 0
+        if self.__verbose:
+            progress(len(column), counter)
+        json = '{\n  "title": "' + feed + '",\n  "news": ['
+        separ = False
+        for news in column:
+            if separ:
+                json += ','
+            separ = True
+            json += '\n    {\n      "title": "' + news[0] + '",'
+            json += '\n      "date": "' + news[1] + '",'
+            json += '\n      "link": "' + news[2] + '",'
+            json += '\n      "description": "' + news[3] + '",'
+            json += '\n      "links": ['
+            links = ""
+            for lin in news[4]:
+                links += f'\n        "{lin}",'
+            json += links[:-1] + "\n      ]" + "\n    }"
+            counter += 1
+            if self.__verbose:
+                progress(len(column), counter)
+        json += '\n  ]\n}'
+        output(json)
