cce26c199c1077ccc008be23502de4eae35c35d9
KirylDv <dkirill111@gmail.com>
2019-11-16 23:55:45 +0300

    1st Iteration

--- rss_reader.py
+++ rss_reader.py
@@ -5,7 +5,7 @@ import reader
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--version", action='version',    help="Print version info", version="Version 0.01")
+    parser.add_argument("--version", action='version',    help="Print version info", version="Version 0.1")
     parser.add_argument("source",    type=str,            help="RSS URL")
     parser.add_argument("--json",    action="store_true", help="Print result as JSON in stdout")
     parser.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
