dec51ec84ad3454e48f07d1da19bdc1d5ffdb386
Yana Shcherbich <vilikdf@gmail.com>
2019-11-17 17:41:05 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='rss-reader',
+    version='2.0',
+    description='Pure Python command-line RSS reader',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/yanaShcherbich/PythonHomework',
+    author='Yana Shcherbich',
+    author_email='vilikdf@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=['feedparser', 'bs4'],  # Optional
+
+
+    entry_points={  # Optional
+        'console_scripts': ['rss-reader=rss.rss_reader:main'],
+    }
+)
