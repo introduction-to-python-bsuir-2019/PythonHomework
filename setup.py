772c22be62a583bc7116257d3482e2ca6281112f
ilyaborodin <ilya.borodin8@gmail.com>
2019-11-17 19:57:59 +0300

    Переменовал команду в rss-reader

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=['feedparser==5.2.1'],
     python_requires='>=3.8',
     entry_points={
-        "console_scripts": ["rss_reader=App.rss_reader:main"],
+        "console_scripts": ["rss-reader=App.rss_reader:main"],
     }
 
 )
