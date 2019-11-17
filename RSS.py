9d4d61f1e980bb965e0e5d1f777293abb2b923ad
ge2nadiy <qwerty@example.com>
2019-11-17 19:29:06 +0300

    add arguments command-line

--- RSS.py
+++ RSS.py
@@ -1,27 +1,39 @@
 import feedparser
+from bs4 import BeautifulSoup
+import json
 
 
 class RssAggregator():
     feedurl = ""
 
-    def __init__(self, paramrssurl):
-        print("links: ", paramrssurl)
-        self.feedurl = paramrssurl
-        self.parse()
+    def __init__(self, args, log):
+        self.args=args
+        self.log=log
 
-    def parse(self):
-        thefeed = feedparser.parse(self.feedurl)
-      
-        print('Number of RSS posts :', len(thefeed.entries))
+    def get_news(self):
+        self.log.info("Getting rss feed")
+        thefeed = feedparser.parse(self.args.source)      
+        return thefeed.entries[:self.args.limit]        
 
-        print("Feed: ", thefeed.feed.get("title", ""))
-      
-        for thefeedentry in thefeed.entries:
+    def print_news(self, entries):
+        self.log.info("Printing news")      
+        for thefeedentry in entries:
             print("--------------------------------------------------")        
-            print("Title: ", thefeedentry.get("title", ""))
-            print("Date: ", thefeedentry.get("published",""))
-            print("Link: ", thefeedentry.get("link", ""))
-            print(thefeedentry.get("description", ""))
+            print("Title: ", thefeedentry.title)
+            print("Date: ", thefeedentry.published)
+            print("Link: ", thefeedentry.link)
+            print(BeautifulSoup(thefeedentry.description, "html.parser").text)  
+
+    def print_json(self, entries):
+        self.log.info("RSS feed to json")
+        for thefeedentry in entries:
+            news={
+                "Title": thefeedentry.title,
+                "Date": thefeedentry.published,
+                "Link": thefeedentry.link,
+                "Discription": BeautifulSoup(thefeedentry.description, "html.parser").text
+            }
+            print("--------------------------------------------------") 
+            print(json.dumps(news, indent=3))
+
             
-            
-        
--- main.py
+++ main.py
@@ -1,4 +1,46 @@
+import argparse
 from RSS import RssAggregator
+import logging
 
-rssobject=RssAggregator("https://timesofindia.indiatimes.com/rssfeedstopstories.cms")
 
+__version__="0.1.0"
+
+def get_args():        
+    parser=argparse.ArgumentParser(description="Pure Python command-line RSS reader.")
+    parser.add_argument('source', help="RSS URL")
+    parser.add_argument("-v","--version", action="version", version="%(prog)s version {version}".format(version=__version__), default=None, help="Print version info")
+    parser.add_argument("--json",action="store_true", help="Print result as JSON in stdout")
+    parser.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
+    parser.add_argument("--limit", type=int, default=None, help="Limit news topics if this parameter provided")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args=get_args()
+    if args.verbose:
+        logger = get_log()
+    else:
+        logger = logging.getLogger()
+    rssobject=RssAggregator(args, logger)
+    news=rssobject.get_news()    
+    if args.version:
+        print(args.version)
+    if args.json:
+        rssobject.print_json(news)
+    else:
+        rssobject.print_news(news) 
+    
+    logger.info("Exit")
+     
+
+def get_log():    
+    logger = logging.getLogger(__name__)
+    logger.level = logging.DEBUG
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+    
+if __name__=="__main__":
+    main() 
