00b6bf7ba3af43dfe099e1e6b5a9ecf155cdc91b
ilyaborodin <ilya.borodin8@gmail.com>
2019-11-17 18:52:34 +0300

    Выполнил 2-ую итерацию
    Добавлен файл setup.py
    Добавлен метож main
    Изменена версия программы на "2.0"

--- App/rss_reader.py
+++ App/rss_reader.py
@@ -3,9 +3,14 @@ import logging
 from App.RSSListener import RSSListener
 from App import Args_parser
 
-if __name__ == '__main__':
+
+def main():
     args = Args_parser.parsing_args()
     Args_parser.start_settings(args)
     logging.info("Program launch")
     rss_listener = RSSListener(args.limit, args.json)
     rss_listener.start(args.source)
+
+
+if __name__ == '__main__':
+    main()
