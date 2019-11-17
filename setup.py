f6660c7bbecc4199b1b471147aea200898ec2a81
kisant <kiselevichanton@gmail.com>
2019-11-17 23:06:09 +0300

    Add setup.py for iteration_2.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="rss-reader",
+    version="1.0",
+    author="Anton Kiselevich",
+    author_email="kiselevichanton@gmail.com",
+    description="Simple command line rss-reader.",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
