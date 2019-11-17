aa4f18280ef8e4141775580d365866f3e175f66d
Zavxoz <artem.klimec8@gmail.com>
2019-11-17 23:42:38 +0300

    adding installing required packages into setup.py

--- rss_reader/__init__.py
+++ rss_reader/__init__.py
@@ -1,2 +1,3 @@
 from .rss_reader import RssReader
 #from .news import News
+__version__ = '1.2'
--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
+from rss_reader import __version__ as version
 
+
+with open('requirements.txt') as f:
+    requirements = f.read()
+    
 setup(
     name='rss-reader',
-    version='1.2',
+    version=version,
     packages=['rss_reader'],
     url='',
     license='',
     author='Zavxoz',
     author_email='artem.klimec8@gmail.com',
     description='simple command-line rss reader',
+    install_requires=requirements,
     entry_points={
         'console_scripts': ['rss-reader = rss_reader.cli:main']
     }
