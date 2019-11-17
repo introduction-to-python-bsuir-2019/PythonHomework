c3bd27ca7d06b9efb6e91bd17c86b87bec881419
Andrey Nenuzhny <andrey@dohop.com>
2019-11-17 17:45:46 +0300

    Fixed paths in pycodestyle.sh script and corrected README.md

--- pycodestyle.sh
+++ pycodestyle.sh
@@ -8,13 +8,13 @@ NC='\033[0m' # No Color
 echo -e "${YELLOW}Starting static code analys ${NC}"
 
 # utils/
-python3 -m pycodestyle utils/ --max-line-length=120
+python3 -m pycodestyle rss_reader/utils/ --max-line-length=120
 echo -e "${YELLOW}utils/ ${GREEN}PASSED${NC}"
 
 # rss.py
-python3 -m pycodestyle rss.py --max-line-length=120
+python3 -m pycodestyle rss_reader/rss.py --max-line-length=120
 echo -e "${YELLOW}rss.py/ ${GREEN}PASSED${NC}"
 
 # bots/
-python3 -m pycodestyle bots/ --max-line-length=120
+python3 -m pycodestyle rss_reader/bots/ --max-line-length=120
 echo -e "${YELLOW}bots/ ${GREEN}PASSED${NC}"
