3b241b5341d8c15d7c66579ce8bf3df1f7c4bf98
elaskin <mr.elaskin@mail.ru>
2019-11-17 23:12:51 +0300

    Edit setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 DESCRIPTON = 'RSS reader is a command-line utility, \
 which receives RSS URL and prints results in human-readable format.'
 
-setup(name='rss-reader',
-	version='1.0',
+setup(name='rss_reader',
+	version='4.0',
 	description=DESCRIPTON,
 	url='#',
 	author='KozachenkoKirill',
 	author_email='mr.elaskin@mail.ru',
-	license='EPAM-Introduction-to-Python',
+	license='EPAM-Introduction-to-Python-2019',
 	packages=['RSS-Reader'],
 	zip_safe=False
 )
