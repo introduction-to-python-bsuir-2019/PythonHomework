a6a297a379a41a56a1475690ee98ef4fac0fdcb7
kisant <kiselevichanton@gmail.com>
2019-11-25 15:28:19 +0300

    Moving part of a code from rss_reader to main

--- Iteration_1/rss_reader.py
+++ Iteration_1/rss_reader.py
@@ -1,4 +1,3 @@
-import argparse
 import feedparser
 from bs4 import BeautifulSoup
 # https://news.yahoo.com/rss/
@@ -42,34 +41,3 @@ class RSSReader:
             item['image_link'] = soup.find('img')['src']
 
         return item
-
-
-def print_to_console(channel_title, feeds):
-    print()
-    print('Feed:', channel_title)
-    print('-' * 40)
-    for item in feeds:
-        print('Title:', item['title'])
-        print('Date:', item['date'])
-        print('Link:', item['link'])
-        print()
-        print('Image title:', item['image_title'])
-        print('Image description:', item['image_description'])
-        print('Image link:', item['image_link'])
-        print('-' * 40)
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Pure Python command-line RSS reader.')
-    parser.add_argument('source', type=str, help='RSS URL')
-    parser.add_argument('--limit', type=int, help='Limit news topics if this parameter provided')
-
-    args = parser.parse_args()
-
-    reader = RSSReader(**vars(args))
-    channel_title, feeds = reader.parse_source()
-    print_to_console(channel_title, feeds)
-
-
-if __name__ == '__main__':
-    main()
