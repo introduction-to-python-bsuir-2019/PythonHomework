6c82b051b431bed6b655179ab00cec674717f747
Aliaksei Dzemasiuk <mr.glazik@gmail.com>
2019-11-26 18:02:37 +0300

    Code polished according to PEP8

--- rss_reader/rss_reader.py
+++ rss_reader/rss_reader.py
@@ -2,7 +2,6 @@ import argparse
 from bs4 import BeautifulSoup
 from datetime import datetime
 import feedparser
-from fpdf import FPDF
 from functools import reduce
 import html
 import jsonpickle
@@ -11,59 +10,60 @@ import os
 import re
 import requests
 
-VERSION='v1.3'
+VERSION = 'v1.4'
 
-class NewsFeed:        
+
+class NewsFeed:
     '''A class that represents news from a specific source. Includes source name and list of retrieved news.'''
     def __init__(self, link):
         '''If link is provided, this method tries to retrieve news and source name from it, does nothing if None'''
         if link:
             logging.info('Retrieving news...')
-            feed=feedparser.parse(link)
-            soup=requests.get(link).text
-            soup=BeautifulSoup(soup,"html5lib")
-            news_separated=soup('item')
+            feed = feedparser.parse(link)
+            soup = requests.get(link).text
+            soup = BeautifulSoup(soup, "html5lib")
+            news_separated = soup('item')
             logging.info('News retrieved, converting to readable format...')
-            self.title=feed['feed']['title']
-            newsdict=feed['entries']
-            self.news=[NewsItem(item, news_separated[index]) for index, item in enumerate(newsdict)]
+            self.title = feed['feed']['title']
+            newsdict = feed['entries']
+            self.news = [NewsItem(item, news_separated[index]) for index, item in enumerate(newsdict)]
 
     def steal_title(self, subject):
         '''Copies source name from an existing object.'''
-        self.title=subject.title
-    
+        self.title = subject.title
+
     def steal_news(self, subject):
         '''Inserts a given list of news into object's news list'''
-        self.news=[]
+        self.news = []
         for item in subject:
             self.news.append(NewsItem())
-            self.news[-1].title=item.title
-            self.news[-1].source=item.source
-            self.news[-1].date=item.date
-            self.news[-1].content=item.content
-            self.news[-1].images=[image for image in item.images]
+            self.news[-1].title = item.title
+            self.news[-1].source = item.source
+            self.news[-1].date = item.date
+            self.news[-1].content = item.content
+            self.news[-1].images = [image for image in item.images]
 
 
 class NewsItem:
     '''Represents a certain news article'''
-    def __init__(self,*args):
+    def __init__(self, *args):
         """Either takes none (creates an "empty" object) or 2 arguments (using more won't change anything)
 
         In case of 2 arguments takes feedparser and BeautifulSoup items.
         feedparser item is used to fill in content while BeautifulSoup is used to search for additional images
         """
         if args:
-            self.title=ultimately_unescape(args[0]['title'])
-            self.source=ultimately_unescape(args[0]['link'])
-            self.date=ultimately_unescape(args[0]['published'])
-            self.content=ultimately_unescape(hide(args[0]['summary'],'<','>'))
-            self.images=[ultimately_unescape(link['href']) for link in args[0]['links'] if 'image' in link['type']]
-            self.images+=find_images(ultimately_unescape(str(args[1])))
+            self.title = ultimately_unescape(args[0]['title'])
+            self.source = ultimately_unescape(args[0]['link'])
+            self.date = ultimately_unescape(args[0]['published'])
+            self.content = ultimately_unescape(hide(args[0]['summary'], '<', '>'))
+            self.images = [ultimately_unescape(link['href']) for link in args[0]['links'] if 'image' in link['type']]
+            self.images += find_images(ultimately_unescape(str(args[1])))
             try:
-                self.images+=[media['url'] for media in args[0]['media_content'] if 'image' in media['type']]
+                self.images += [media['url'] for media in args[0]['media_content'] if 'image' in media['type']]
             except KeyError:
                 pass
-            self.images=[image for index, image in enumerate(self.images) if self.images.index(image)==index and image]
+            self.images = [img for index, img in enumerate(self.images) if self.images.index(img) == index and img]
 
     def show_fields(self):
         '''Prints all the fields this object has'''
@@ -85,18 +85,20 @@ def append_existing(appendee, frame, content):
     appendee[-1].steal_news(content)
     return appendee
 
+
 def cut_news_off(limit, feed):
     '''Implements logic of --limit argument, also works with a bunch of items from different sources'''
-    total_news=reduce(lambda x,y: x+y, [len(item.news) for item in feed])
-    if not limit or total_news<limit:
+    total_news = reduce(lambda x, y: x+y, [len(item.news) for item in feed])
+    if not limit or total_news < limit:
         return feed
-    left_out=0
-    while total_news>=limit:
-        left_out-=1
-        total_news=reduce(lambda x,y: x+y, [len(item.news) for item in feed[:left_out]]) if feed[:left_out] else 0
-    news_to_add=feed[left_out].news[:limit-total_news]
-    new_feed=feed[:left_out]
-    return append_existing(new_feed,feed[left_out], news_to_add)
+    left_out = 0
+    while total_news >= limit:
+        left_out -= 1
+        total_news = reduce(lambda x, y: x+y, [len(item.news) for item in feed[:left_out]]) if feed[:left_out] else 0
+    news_to_add = feed[left_out].news[:limit-total_news]
+    new_feed = feed[:left_out]
+    return append_existing(new_feed, feed[left_out], news_to_add)
+
 
 def date_to_filename(news):
     '''Converts pubdate to %Y%m%d format; it is used as a file name for caching'''
@@ -105,16 +107,18 @@ def date_to_filename(news):
     except ValueError:
         return datetime.strptime(news.date, '%d %b %Y %H:%M:%S %z').strftime('%Y%m%d')
 
+
 def find_images(text):
     '''Takes BeautifulSoup representation of a news article as an argument and searches for additional images'''
     logging.info('Searching for additional images...')
-    res=[]
-    occurences=re.finditer('<img',text)
-    tags=[(text.rfind('<',0,item.start()+2),text.find('>',item.start()+2)) for item in occurences]
-    where_links_start=[text.find('src',start,end)+5 for start, end in tags]
-    borders=[(start,text.find('"',start)) for start in where_links_start]
-    res+=[text[opener:closer] for opener, closer in borders]
+    res = []
+    occurences = re.finditer('<img', text)
+    tags = [(text.rfind('<', 0, item.start()+2), text.find('>', item.start()+2)) for item in occurences]
+    where_links_start = [text.find('src', start, end)+5 for start, end in tags]
+    borders = [(start, text.find('"', start)) for start in where_links_start]
+    res += [text[opener:closer] for opener, closer in borders]
     return res
+
 
 def get_all_filenames(news):
     """Takes a NewsFeed object as an argument
@@ -122,22 +126,25 @@ def get_all_filenames(news):
     """
     return list(set(date_to_filename(item) for item in news))
 
+
 def hide(text, starts_with, ends_with):
     '''Gets rid of parts of text that shouldn't be visible (like HTML tags)'''
-    return re.sub(re.compile(starts_with+'.*?'+ends_with),'',text)
+    return re.sub(re.compile(starts_with+'.*?'+ends_with), '', text)
+
 
 def insert_cache(cache_files, cache):
     '''Writes newly formed cache to respective files'''
     jsonpickle.set_encoder_options('json', ensure_ascii=False)
     for date, cached_content in zip(cache_files, cache):
-        with open('cache/'+date+'.json','w') as cache:
+        with open('cache/'+date+'.json', 'w') as cache:
             cache.write(jsonpickle.encode(cached_content))
+
 
 def make_fb2(news):
     '''Creates an .fb2 file that contains news designed for output'''
     logging.info('Creating an FB2 file...')
-    filename=os.path.join(os.path.expanduser('~/Desktop'),datetime.now().strftime('%H%M%S%b%d%Y'))
-    with open(filename+'.fb2','w') as filer:
+    filename = os.path.join(os.path.expanduser('~/Desktop'), datetime.now().strftime('%H%M%S%b%d%Y'))
+    with open(filename+'.fb2', 'w') as filer:
         filer.write('<?xml version="1.0" encoding="UTF-8"?><FictionBook><description></description><body>')
         try:
             for source in news:
@@ -157,17 +164,19 @@ def make_fb2(news):
                 filer.write('<p>'+source+'</p>')
         filer.write('</body></FictionBook>')
 
+
 def make_json(news):
     '''Converts news to JSON format'''
     logging.info('Converting to JSON...')
     jsonpickle.set_encoder_options('json', ensure_ascii=False)
     return [jsonpickle.encode(item) for item in news]
 
+
 def make_html(news):
     '''Creates an .html file that contains news designed for output'''
     logging.info('Creating an HTML file...')
-    filename=os.path.join(os.path.expanduser('~/Desktop'),datetime.now().strftime('%H%M%S%b%d%Y'))
-    with open(filename+'.html','w') as filer:
+    filename = os.path.join(os.path.expanduser('~/Desktop'), datetime.now().strftime('%H%M%S%b%d%Y'))
+    with open(filename+'.html', 'w') as filer:
         filer.write('<html>\n<head></head><body>')
         try:
             for source in news:
@@ -187,19 +196,21 @@ def make_html(news):
         filer.write('</body></html>')
     print('All news can be found in '+filename+'.html on your desktop')
 
+
 def parse_arguments():
     '''Parses arguments from command line'''
-    parser=argparse.ArgumentParser(description='Pure Python command-line RSS reader.')
-    exclusive=parser.add_mutually_exclusive_group()
-    parser.add_argument('--json',action='store_true', help='Print result as JSON in stdout')
-    parser.add_argument('--verbose',action='store_true',help='Outputs verbose status messages')
-    parser.add_argument('--limit', type=int,action='store',help='Limit news topics if this parameter provided')
-    parser.add_argument('--to-html', action='store_true',help='Creates an .html file with news designed to be printed')
+    parser = argparse.ArgumentParser(description='Pure Python command-line RSS reader.')
+    exclusive = parser.add_mutually_exclusive_group()
+    parser.add_argument('--json', action='store_true', help='Print result as JSON in stdout')
+    parser.add_argument('--verbose', action='store_true', help='Outputs verbose status messages')
+    parser.add_argument('--limit', type=int, action='store', help='Limit news topics if this parameter provided')
+    parser.add_argument('--to-html', action='store_true', help='Creates an .html file with news designed to be printed')
     parser.add_argument('--to-fb2', action='store_true', help='Creates an .fb2 file with news designed to be printed')
-    exclusive.add_argument('--version',action='store_true',help='Print version info')
-    exclusive.add_argument('source',nargs='?',help='RSS URL',default=None)
+    exclusive.add_argument('--version', action='store_true', help='Print version info')
+    exclusive.add_argument('source', nargs='?', help='RSS URL', default=None)
     exclusive.add_argument('--date', type=str, action='store', help='Print news posted at a certain date')
     return parser.parse_args()
+
 
 def print_news(news):
     '''Prints any content that's designed to be printed'''
@@ -209,58 +220,62 @@ def print_news(news):
             for item in source.news:
                 item.show_fields()
     except TypeError:
-        print(list(map(lambda x: hide(x,'"py/object":','", '), news)))
+        print(list(map(lambda x: hide(x, '"py/object":', '", '), news)))
+
 
 def retrieve_cached_news(filenames):
     '''Reads cache from all files mentioned in filenames'''
     logging.info('Retrieving cached news...')
-    cached=[]
+    cached = []
     for filename in filenames:
         try:
-            with open('cache/'+filename+'.json','r') as cache:
+            with open('cache/'+filename+'.json', 'r') as cache:
                 cached.append(jsonpickle.decode(cache.read()))
         except FileNotFoundError:
-            with open('cache/'+filename+'.json','w') as cache:
+            with open('cache/'+filename+'.json', 'w') as cache:
                 cache.write(jsonpickle.encode([]))
             cached.append([])
     return cached
 
+
 def ultimately_unescape(text):
     '''Unescapes HTML characters multiple times (turned out to be an issue with some sources)'''
     logging.info('Unescaping HTML characters...')
-    while html.unescape(text)!=text:
-        text=html.unescape(text)
+    while html.unescape(text) != text:
+        text = html.unescape(text)
     return text
+
 
 def update_cache(fresh_news, dates, cached):
     '''Appends retrieved news to existing cache'''
     logging.info('Caching retrieved news...')
     for date, cached_content in zip(dates, cached):
-        news_to_append=[item for item in fresh_news[0].news if date_to_filename(item)==date]
-        cached_source=None
+        news_to_append = [item for item in fresh_news[0].news if date_to_filename(item) == date]
+        cached_source = None
         for source in cached_content:
-            if source.title==fresh_news[0].title:
-                cached_source=source
+            if source.title == fresh_news[0].title:
+                cached_source = source
         if cached_source:
-            cached_links=[item.source for item in cached_source.news]
-            cached_source.news+=[news for news in news_to_append if news.source not in cached_links]
+            cached_links = [item.source for item in cached_source.news]
+            cached_source.news += [news for news in news_to_append if news.source not in cached_links]
         else:
-            cached_content=append_existing(cached_content, fresh_news[0], news_to_append)
+            cached_content = append_existing(cached_content, fresh_news[0], news_to_append)
     return cached
-               
+
+
 def main():
-    args=parse_arguments()
+    args = parse_arguments()
     if args.version and (args.json or args.limit or args.topdf):
         raise ValueError('You don\'t use --version together with other arguments')
     if not (args.version or args.source or args.date):
         raise ValueError('Source, --date or --version expected')
-    if args.limit and args.limit<1:
+    if args.limit and args.limit < 1:
         raise ValueError('Incorrect limit input (likely to be non-positive)')
     if args.version:
         print('RSS-reader '+VERSION)
     else:
         if args.verbose:
-            logging.basicConfig(level=logging.INFO, 
+            logging.basicConfig(level=logging.INFO,
                                 format='%(asctime)s - %(message)s',
                                 datefmt='%H:%M:%S')
         try:
@@ -268,30 +283,31 @@ def main():
             logging.info('Cache folder successfully created')
         except FileExistsError:
             pass
-        feed=[]
+        feed = []
         if args.date:
             try:
                 datetime.strptime(args.date, '%Y%m%d')
-                with open('cache/'+args.date+'.json','r') as cache:
-                    feed=retrieve_cached_news([args.date])[0]
+                with open('cache/'+args.date+'.json', 'r') as cache:
+                    feed = retrieve_cached_news([args.date])[0]
             except ValueError:
                 raise ValueError('Incorrect date input')
             except FileNotFoundError:
                 raise FileNotFoundError('There is no cached news for this date')
         else:
-            feed=[NewsFeed(args.source)]
-            cache_files=get_all_filenames(feed[0].news)
-            cached=retrieve_cached_news(cache_files)
-            cached=update_cache(feed, cache_files, cached)
+            feed = [NewsFeed(args.source)]
+            cache_files = get_all_filenames(feed[0].news)
+            cached = retrieve_cached_news(cache_files)
+            cached = update_cache(feed, cache_files, cached)
             insert_cache(cache_files, cached)
-        feed=cut_news_off(args.limit, feed)
+        feed = cut_news_off(args.limit, feed)
         if args.json:
-            feed=make_json(feed)
+            feed = make_json(feed)
         if args.to_html:
             make_html(feed)
         if args.to_fb2:
             make_fb2(feed)
         print_news(feed)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 import setuptools
 from rss_reader.rss_reader import VERSION
 
-setuptools.setup(name='adzemreader',
-                version=VERSION[1:],
-                author='Aliaksei Dzemasiuk',
-                author_email='mr.glazik@gmail.com',
-                description='RSS-feed reader',
-                long_description='Pure Python command line RSS-feed reader ',
-                packages=setuptools.find_packages(),
-                classifiers=["Programming Language :: Python :: 3",
-                            "Operating System :: OS Independent"],
-                python_requires='>=3.5',
-                entry_points={'console_scripts': ['rssreader=rss_reader.rss_reader:main']},
-                install_requires=['bs4',
-                                 'feedparser',
-                                 'jsonpickle'])       
+with open('requirements.txt') as f:
+    requirements=f.read().splitlines()
 
+setuptools.setup(name='adzemreader',
+                 version=VERSION[1:],
+                 author='Aliaksei Dzemasiuk',
+                 author_email='mr.glazik@gmail.com',
+                 description='RSS-feed reader',
+                 long_description='Pure Python command line RSS-feed reader ',
+                 packages=setuptools.find_packages(),
+                 classifiers=["Programming Language :: Python :: 3",
+                              "Operating System :: OS Independent"],
+                 python_requires='>=3.5',
+                 entry_points={'console_scripts': ['rssreader=rss_reader.rss_reader:main']},
+                 install_requires=['html5lib',
+                                   'requests',
+                                   'jsonpickle',
+                                   'bs4',
+                                   'feedparser'],
+                 setup_requires=['html5lib',
+                                 'requests',
+                                 'jsonpickle',
+                                 'bs4',
+                                 'feedparser']
+)
