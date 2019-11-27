7f0a3b9a1613dbee645a6e50899c9e3fa8d7e64d
kisant <kiselevichanton@gmail.com>
2019-11-27 12:08:22 +0300

    Change package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rss-reader-kis",
-    version="1.1",
+    version="1.2",
     author="Anton Kiselevich",
     author_email="kiselevichanton@gmail.com",
     description="Simple command line rss-reader.",
