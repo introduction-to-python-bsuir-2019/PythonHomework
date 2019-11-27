59a3f0d478127f930779f47446ed33710904b2c5
Vlad Bubeniuk <ZayByst@mail.ru>
2019-11-27 16:33:40 +0300

    Change host in mongo

--- rss_reader_ft/db/mongodb_config.py
+++ rss_reader_ft/db/mongodb_config.py
@@ -1,4 +1,4 @@
 """This modules contains internal database configuration"""
-URL_CONNECTION = "mongodb://localhost:27017/"
+URL_CONNECTION = "mongodb://mongo:27017/"
 DB_NAME = "News_feed"
 COLLECTION_NAME = "feeds"
