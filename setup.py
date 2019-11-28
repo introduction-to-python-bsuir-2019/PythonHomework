0d5abea18afbe865f7b1c14d62eb8594e4568c39
Nikita Efremov <nikefremov.00.008@hotmail.com>
2019-11-29 01:20:38 +0300

    Fix setup.py for automation testing

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import reader
 setup(
     name='reader',
     packages=find_packages(),
-    install_requires=["logging", "argparse", "beautifulsoup4", "lxml"],
+    install_requires=["argparse", "beautifulsoup4", "lxml"],
     entry_points={
         'console_scripts':
             ["rss-reader = reader.rss:main"]
