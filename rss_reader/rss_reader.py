ee431b9b065873e2e6fb3dedb12ca5e9801b7e91
Elia Onishchouck <elias0n@mail.ru>
2019-11-28 15:23:10 +0300

    Now news can be cached and loaded using --date argument

--- rss_reader/rss_reader.py
+++ rss_reader/rss_reader.py
@@ -3,45 +3,69 @@ import feedparser
 import logging
 import html
 import json
+from pathlib import Path
 from bs4 import BeautifulSoup
-# from tqdm import tqdm
-from rss_reader import version as vers
-# import version as vers
+# from rss_reader import version as vers
+import version as vers
 
 
-class News_Feed:
+class NewsFeed:
+    """Base class for news feed"""
     def __init__(self, feed_title, items):
         self.feed_title = feed_title
         self.items = items
 
-    def print_to_json(self):
+    def print_to_json(self, limit):
         logging.info('Printing news in json format')
-        print(json.dumps({"Feed": self.feed_title, "Items": [item.return_item() for item in self.items]}))
+        print(json.dumps(self.create_json(0, limit)))
 
-    def print_to_console(self):
+    def create_json(self, is_cached, limit):
+        return {'Feed': self.feed_title, 'Items': [item.return_item(is_cached) for item in self.items[:limit]]}
+
+    def print_to_console(self, limit):
         logging.info('Printing news in console format')
         print('Feed: {0}'.format(self.feed_title))
-        for item in self.items:
+        for item in self.items[:limit]:
             item.print_to_console()
+        logging.info('Printed %s news', limit)
 
-    def print_feed(self, json):
+    def print_feed(self, json, limit):
+        if limit > len(self.items) or limit < 0:
+            limit = len(self.items)
         if(json):
-            self.print_to_json()
+            self.print_to_json(limit)
         else:
-            self.print_to_console()
+            self.print_to_console(limit)
+
+    def save_news(self, limit):
+        logging.info('Saving news')
+        news_to_save = self.create_json(1, limit)['Items']
+        existing_news = load_from_cache()
+        news_to_save += [item for item in existing_news if not item in news_to_save]
+        with open('cache.json', 'w') as json_file:
+            json.dump(news_to_save, json_file)
 
 
 class Item:
-    def __init__(self, title, date, link, description, links):
-        self.title = title
-        self.date = date
-        self.link = link
-        self.description = description
-        self.links = links
+    """
+    Class for single news item from news feed
+    Attributes of the class are:
+        title          a
+        pubDate        a
+        link           a
+        description    a
+        links          a
+        date_string    a
+        source         a
+    """
+    def __init__(self, news_dict):
+        for key in news_dict:
+            setattr(self, key, news_dict[key])
+
 
     def print_to_console(self):
         print('\nTitle: {0}'.format(self.title))
-        print('Date: {0}'.format(self.date))
+        print('Date: {0}'.format(self.pubDate))
         print('Link: {0} \n'.format(self.link))
         print(self.description)
         print()
@@ -62,9 +86,14 @@ class Item:
                 print(link)
         print('\n//////////////////////////////////////////////////////////////////////////')
 
-    def return_item(self):
-        return {"title": self.title, "description": self.description,
-                "link": self.link, "pubDate": self.date, "source": self.links}
+    def return_item(self, is_cached):
+        item_content = {'title': self.title, 'description': self.description,
+                        'link': self.link, 'pubDate': self.pubDate, 'links': self.links}
+        if is_cached:
+            item_content['date_string'] = self.date_string
+            item_content['source'] = self.source
+
+        return item_content
 
 
 def set_argparse():
@@ -75,6 +104,7 @@ def set_argparse():
     parser.add_argument('--json', action='store_true', help='Print result as JSON in stdout')
     parser.add_argument('--verbose', action='store_true', help='Outputs verbose status messages')
     parser.add_argument('--limit', type=int, default=-1, help='Limit news topics if this parameter provided')
+    parser.add_argument('--date', type=str, help='Shows news of specific date')
     return parser.parse_args()
 
 
@@ -131,6 +161,26 @@ def find_videos(args, soup):
     return video_links
 
 
+def read_from_cache(date, source):
+    cached_news = load_from_cache()
+    dated_news = []
+    for news in cached_news:
+        if news['source'] == source and news['date_string'] == date:
+            dated_news.append(Item(news))
+    return dated_news
+
+
+def load_from_cache():
+    logging.info('Loading from cache')
+    cached_news = []
+    if Path('cache.json').is_file():
+        with open("cache.json") as cache:
+            data = cache.read()
+            cached_news = json.loads(data)
+    logging.info('Loaded %s news', len(cached_news))
+    return cached_news
+
+
 def main():
     try:
         args = set_argparse()
@@ -138,34 +188,45 @@ def main():
             logging.basicConfig(format='%(asctime)s %(funcName)s %(message)s', datefmt='%I:%M:%S', level=logging.DEBUG)
 
         logging.info('Application started. RSS source is %s', args.source)
-        NewsFeed = feedparser.parse(args.source)
-        if NewsFeed.bozo == 1:
-            raise Exception('The feed is not well-formed XML')
-        # if 'status' not in NewsFeed:
-        #    raise Exception('An error happened such that the feed does not contain an HTTP response')
-        if args.limit < 0 or args.limit > len(NewsFeed.entries):
-            args.limit = len(NewsFeed.entries)
+        args.source = args.source.rstrip('/')
 
-        news = []
-        logging.info('Begin processing each news')
-        for i in range(args.limit):
-            logging.info('Parsing news number %s', i+1)
-            entry = NewsFeed.entries[i]
-            soup = html.unescape(BeautifulSoup(entry['summary'], "html.parser"))
-            images_links = find_images(args, soup)
-            href_links = find_href(args, soup)
-            video_links = find_videos(args, soup)
-            links = {'images_links': images_links, 'href_links': href_links, 'video_links': video_links}
-            news.append(Item(html.unescape(entry['title']), entry['published'], entry['link'], html.unescape(soup.text), links))
-            logging.info('News number %s has parsed', i+1)
+        if args.date:
+            news = read_from_cache(args.date, args.source)
+            if not news:
+                raise Exception('The are no news of {0} from {1} stored'.format(args.source, args.date))
+            news_feed = NewsFeed('Cached news', news)
+        else:
+            parsed_feed = feedparser.parse(args.source)
+            if parsed_feed.bozo == 1:
+                raise Exception('The feed is not well-formed XML. Details are {0}'.format(parsed_feed.bozo_exception))
+            # if 'status' not in NewsFeed:
+            #    raise Exception('An error happened such that the feed does not contain an HTTP response')
+            news = []
+            logging.info('Begin processing each news')
+            for i in range(len(parsed_feed.entries)):
+                logging.info('Parsing news number %s', i + 1)
+                entry = parsed_feed.entries[i]
+                soup = html.unescape(BeautifulSoup(entry['summary'], 'html.parser'))
+                images_links = find_images(args, soup)
+                href_links = find_href(args, soup)
+                video_links = find_videos(args, soup)
+                links = {'images_links': images_links, 'href_links': href_links, 'video_links': video_links}
+                date_string = ''.join(map(str, entry.published_parsed[:3]))
+                dict_news = {'title': html.unescape(entry['title']), 'pubDate': entry['published'], 'link': entry['link'],
+                             'description': html.unescape(soup.text), 'links': links, 'date_string': date_string,
+                             'source': args.source}
+                news.append(Item(dict_news))
+                logging.info('News number %s has parsed', i + 1)
+            news_feed = NewsFeed(parsed_feed.feed.title, news)
+            news_feed.save_news(len(parsed_feed.entries))
+        news_feed.print_feed(args.json, args.limit)
 
-        newsFeed = News_Feed(NewsFeed.feed.title, news)
-        newsFeed.print_feed(args.json)
         logging.info('Application completed')
-        
+
     except Exception as e:
         print(e)
-        
+
+
 if __name__ == '__main__':
 
     main()
