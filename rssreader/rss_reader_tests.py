b009b728146c4b9a8020955564ee487e37f2d6a3
PivovarSergey <pivovar-ser-leon@inbox.ru>
2019-11-28 22:57:29 +0300

    Completed unittests rss_reader_tests

--- rssreader/rss_reader_tests.py
+++ rssreader/rss_reader_tests.py
@@ -1,14 +1,15 @@
 import time
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-from rss_reader import RSSReader
+from rssreader.rss_reader import RSSReader
 
 __all__ = ['TestRSSReader']
 
 
 class TestRSSReader(unittest.TestCase):
     def setUp(self):
+        date = time.struct_time((2019, 11, 26, 20, 53, 11, 1, 330, 0))
         self.reader = RSSReader()
         self.source = "https://news.yahoo.com/rss/"
         self.response = {
@@ -20,7 +21,7 @@ class TestRSSReader(unittest.TestCase):
                 'title': 'Some title',
                 'description': '<p><a href="some long link"><img src="some long link to source of image 2" width="130" height="86" alt="Alt of image 2"></a>Some long description<p><br clear="all">',
                 'link': 'some long link',
-                'published_parsed': time.struct_time((2019, 11, 26, 20, 53, 11, 1, 330, 0)),
+                'published_parsed': date,
             }]
         }
         self.response_parsed = {
@@ -50,42 +51,46 @@ class TestRSSReader(unittest.TestCase):
 
     def test_get_articles_from_url(self):
         limit = 1
-        with patch('rssreader.html_parser.Parser.parse') as rssreader_mock:
-            rssreader_mock.return_value = 'Successful'
+        with patch('rssreader.html_parser.Parser.parse') as html_parser_mock:
+            html_parser_mock.return_value = 'Successful'
             with patch('feedparser.parse') as feedparser_mock:
                 feedparser_mock.return_value = self.response
                 self.assertEqual(self.reader._get_articles_from_url(self.source, limit), 'Successful')
 
         feedparser_mock.assert_called_with(self.source.strip())
-        rssreader_mock.assert_called_with(self.response, limit)
+        html_parser_mock.assert_called_with(self.response, limit)
 
     def test_call_save(self):
         limit = 1
+        self.reader._get_articles_from_url = MagicMock(return_value=self.response_parsed)
         with patch('rssreader.output_controller.OutputController.print') as print_mock:
-            print_mock.return_value = True
-            with patch('rssreader.storage.controller.StorageController.save') as ctrl_save_mock:
-                ctrl_save_mock.return_value = 1
-                with patch('rss_reader.RSSReader._get_articles_from_url') as reader_mock:
-                    reader_mock.return_value = self.response_parsed
-                    self.assertTrue(self.reader(self.source, limit, None))
+
+            # Time for crutches :)
+            with patch('rssreader.storage.controller.StorageController.__init__') as crutch:
+                crutch.return_value = None  # remove creating db file
+                # How to replace a class object on MagicMock without crutch?
+
+                with patch('rssreader.storage.controller.StorageController.save') as storage_mock:
+                    storage_mock.return_value = 1
+                    self.assertIsNone(self.reader(self.source, limit, None))
 
         print_mock.assert_called_with(self.response_parsed)
-        ctrl_save_mock.assert_called_with(self.source,
-                                          self.response_parsed['articles'],
-                                          self.response_parsed['title'])
-        reader_mock.assert_called_with(self.source, limit)
 
     def test_call_load(self):
         limit = 1
         date = '20191122'
         with patch('rssreader.output_controller.OutputController.print') as print_mock:
-            print_mock.return_value = True
-            with patch('rssreader.storage.controller.StorageController.load') as ctrl_load_mock:
-                ctrl_load_mock.return_value = self.response_parsed
-                self.assertTrue(self.reader(self.source, limit, date))
+            # Time for crutches :)
+            with patch('rssreader.storage.controller.StorageController.__init__') as crutch:
+                crutch.return_value = None  # remove creating db file
+                # How to replace a class object on MagicMock without crutch?
 
-        print_mock.assert_called_with(self.response_parsed)
-        ctrl_load_mock.assert_called_with(self.source, date, limit)
+                with patch('rssreader.storage.controller.StorageController.load') as storage_mock:
+                    storage_mock.return_value = self.response_parsed
+                    self.assertIsNone(self.reader(self.source, limit, date))
+
+        print_mock.assert_called_once_with(self.response_parsed)
+        storage_mock.assert_called_with(self.source, date, limit)
 
 
 if __name__ == '__main__':
