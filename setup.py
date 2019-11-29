ee6a77a47fcf59a9d6a5de429a6b8380829596cb
Marta Gulida <gulidamarta@gmail.com>
2019-11-29 09:55:13 +0200

    Updated setup settings

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 with open("README.md") as readme_stream:
-	description = readme_stream.read()
+    description = readme_stream.read()
 
 setuptools.setup(
-    name="Pure Python command line rss-reader",
+    name="rss-reader",
     version=2.0,
     author="Gulida Marta",
     author_email="gulidamarta@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
                 "from the source you will give it.",
     install_requires=['feedparser', 'bs4', 'lxml'],
     packages=setuptools.find_packages(),
-	include_package_data=True,
+    include_package_data=True,
     python_requires='>=3.6',
     entry_points={
         'console_scripts': ['reader=rss_reader.rss_reader:main'],
