68fd97690f2bdfefa657da711a6113fb155a83db
kisant <kiselevichanton@gmail.com>
2019-11-30 11:01:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="rss-reader-kis",
-    version="1.2",
+    version="1.75",
     author="Anton Kiselevich",
     author_email="kiselevichanton@gmail.com",
     description="Simple command line rss-reader.",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    install_requires=[
+        'feedparser==5.2.1',
+        'bs4==0.0.1'
+    ],
+    entry_points={
+        'console_scripts': ['rss-reader=reader.main:main'],
+    }
 )
