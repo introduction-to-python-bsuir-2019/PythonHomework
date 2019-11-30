238a1caccdf32a81a2e826a743cb7febe36a4aa1
kisant <kiselevichanton@gmail.com>
2019-11-30 20:21:26 +0300

    Add --to-html convert

--- reader/main.py
+++ reader/main.py
@@ -8,6 +8,7 @@ def main():
     parser.add_argument('--limit', type=int, help='Limit news topics if this parameter provided')
     parser.add_argument('--json', help='Print result as JSON in stdout', action='store_true')
     parser.add_argument('--date', type=str, help='Print news from cache by date')
+    parser.add_argument('--to-html', type=str, help='Enter the path where new file will be saved')
     args = parser.parse_args()
 
     reader = RSSReader(**vars(args))
