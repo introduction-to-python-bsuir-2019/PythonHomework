e362f8c76ccc183dd09e23c3c47373cbdd039ef8
victormikhan <victormikhan@gmail.com>
2019-11-30 15:12:43 +0300

    add initialization validation to no bool rss-reader param source, limit

--- src/components/parser/arguments/optional/limit.py
+++ src/components/parser/arguments/optional/limit.py
@@ -1,10 +1,20 @@
 from src.components.parser.arguments import ArgumentsAbstract
-
+import argparse
+import sys
 
 class Limit(ArgumentsAbstract):
 
     def add_argument(self):
         self._parser.add_argument(
-            '--limit', type=int, default=3, help='Limit news topics if this parameter provided'
+            '--limit', type=self._validate_limit, default=3, help='Limit news topics if this parameter provided'
         )
 
+    def _validate_limit(self, limit):
+        try:
+            if not int(limit) > 0:
+                raise argparse.ArgumentTypeError
+
+            return int(limit)
+
+        except argparse.ArgumentTypeError:
+            raise argparse.ArgumentTypeError('Argument limit equal or less 0')
--- src/components/parser/arguments/positional/source.py
+++ src/components/parser/arguments/positional/source.py
@@ -1,11 +1,25 @@
 from src.components.parser.arguments.arguments_abstract import ArgumentsAbstract
-
+import argparse
+import urllib.request as url
+import sys
 
 class Source(ArgumentsAbstract):
 
     def add_argument(self):
         self._parser.add_argument(
-            'source', type=str, help='RSS URL'
+            'source', type=self._validate_source, help='RSS URL'
         )
 
+    def _validate_source(self, source):
 
+        try:
+            if url.urlopen(source).getcode() is not 200:
+                raise argparse.ArgumentError
+
+            return source
+
+        except argparse.ArgumentError:
+            raise argparse.ArgumentError('Server answer code is not 200')
+
+        except (url.HTTPError, url.URLError) as e:
+            raise url.URLError(f'Something wrong with your source. Please try another rss feed: {e}')
