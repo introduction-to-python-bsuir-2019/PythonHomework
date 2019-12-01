5318e0b21caf19056181a1655a670e47739565b1
Vlad Bubeniuk <ZayByst@mail.ru>
2019-12-01 23:57:15 +0300

    last commit

--- tests/unit/db_test/db_test.py
+++ tests/unit/db_test/db_test.py
@@ -1,17 +1,1 @@
 """Tests for rss_reader_ft.db module"""
-
-import unittest
-from unittest import mock
-
-from rss_reader_ft.db.mongodb import MongoDatabase
-from rss_reader_ft.db.mongodb_config import URL_CONNECTION, DB_NAME, COLLECTION_NAME
-from tests.unit.db_test.data import NEWS
-
-
-class RssParserTestCase(unittest.TestCase):
-    """Test cases for MongoDatabase class"""
-
-
-if __name__ == "__main__":
-    unittest.main()
-
--- tests/unit/rss_test/rss_test.py
+++ tests/unit/rss_test/rss_test.py
@@ -1,15 +1,1 @@
 """Tests for rss_reader_ft.rss module"""
-import time
-import pprint
-from unittest import mock
-from rss_reader_ft.rss.rss_feed import RSSFeed
-
-class RSSTest():
-
-    @mock.patch('time.time', return_value = 5)
-    def test_func(self, mocker):
-       
-        assert RSSFeed({"source": "https://"}, {}) == True
-
-if __name__ == "__main__":
-    RSSTest.test_func()
