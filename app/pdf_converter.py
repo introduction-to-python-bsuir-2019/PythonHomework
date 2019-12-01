12f905188a7a55afd0da9283f1ad5aef03153f7f
Andery Dydyshko <dydyshko1999@gmail.com>
2019-12-01 23:06:23 +0300

    refactored args
    was -> args
    now -> limit url date etc.

--- app/pdf_converter.py
+++ app/pdf_converter.py
@@ -6,25 +6,30 @@
 import os
 import json
 import warnings
+from datetime import datetime
 
 from bs4 import BeautifulSoup
+import urllib.error
 import urllib.request
 import fpdf
 
 from app.RSSReader import RSSReader
 
 
+# To protect your fragile mind from BeautifulSoup warnings
 warnings.filterwarnings("ignore")
 
 
 class PDFConverter:
     """ Writes news in PDF file """
 
-    def __init__(self, args, logger, news=None):
+    def __init__(self, url, limit, date, to_pdf, logger, news=None):
         """ Sets up fonts for PDF file """
 
-        self.arguments = args
-        self.args = args.get_args()
+        self.url = url
+        self.limit = limit
+        self.date = date
+        self.to_pdf = to_pdf
         self.news = news
         self.logger = logger
 
@@ -38,7 +43,7 @@ class PDFConverter:
 
         self.write_title('Cached RSS news')
 
-        file_path = 'cache' + os.path.sep + self.args.date + '.json'
+        file_path = 'cache' + os.path.sep + self.date + '.json'
         with open(file_path, encoding='utf-8') as rf:
             news = json.load(rf)
 
@@ -47,7 +52,7 @@ class PDFConverter:
             self.pdf.ln(10)
             self.pdf.add_page()
         try:
-            file_path = self.args.to_pdf + os.path.sep + 'cached_news.pdf'
+            file_path = self.to_pdf + os.path.sep + 'cached_news.pdf'
             self.pdf.output(file_path)
         except FileNotFoundError:
             self.logger.info(f'Path to file {file_path} not found')
@@ -62,14 +67,14 @@ class PDFConverter:
 
         self.write_title('RSS news')
 
-        rss_reader = RSSReader(self.arguments, self.logger)
+        rss_reader = RSSReader(self.url, self.limit, self.date, self.logger)
         for new in self.news:
             new = rss_reader.to_dict(new)
             self.create_cells(new)
             self.pdf.ln(10)
             self.pdf.add_page()
         try:
-            file_path = self.args.to_pdf + os.path.sep + 'news.pdf'
+            file_path = self.to_pdf + os.path.sep + 'news.pdf'
             self.pdf.output(file_path)
         except FileNotFoundError:
             self.logger.info(f'Path to file {file_path} not found')
@@ -110,19 +115,22 @@ class PDFConverter:
             self.logger.info('Creating directory images')
             os.mkdir(directory_path)
 
-        img_name = self.create_image_name(img_url)
+        img_name = self.create_image_name()
         try:
             img = urllib.request.urlopen(img_url).read()
         except ValueError:
             self.logger.info('Failed image download')
             return None
+        except urllib.error.URLError:
+            self.logger.info('The attempt to establish a connection was unsuccessful because '
+                             'Due to incorrect response of already connected computer')
 
         try:
             img_path = os.path.abspath('') + os.path.sep + directory_path + img_name
             with open(img_path, "wb") as out:
                 out.write(img)
         except FileNotFoundError:
-            self.logger.info('Could not write image to folder Images')
+            self.logger.info('Could not write image to folder images')
         else:
             self.logger.info('Image has been downloaded')
         return img_path
@@ -138,21 +146,9 @@ class PDFConverter:
             return None
         self.pdf.multi_cell(0, 10, txt=f'{img_path}')
 
-    def create_image_name(self, img_url):
-        """
-            Image URL looks like
-            'https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-images/2019-11/aa8c02a0-0c79-11ea-b1fd-1011ee5d77f0'
-            (without '.jpg' at the end)
-            or
-            'https://img.tyt.by/thumbnails/n/minsk/0a/10/proektnoe_predlozhenie_ulyanovskaya_belorusskaya.jpg'
-            (with '.jpg' at the end)
-            This method gets last part of url after '/' and adds '.jpg' if it is missing
-            So image name will look like 'aa8c02a0-0c79-11ea-b1fd-1011ee5d77f0.jpg'
-        """
+    def create_image_name(self):
+        """ Creates name for image using current time (YearMonthDay_HoursMinutesSeconds_Milliseconds.jpg) """
 
-        split_list = img_url.split('/')
-        img_name = split_list[-1]
-        if '.jpg' in img_name:
-            return img_name
+        img_name = datetime.today().strftime('%Y%m%d_%H%M%S_%f')
         img_name += '.jpg'
         return img_name
