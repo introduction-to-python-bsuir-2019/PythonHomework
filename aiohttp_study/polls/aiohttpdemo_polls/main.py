635812688c5332aef8ad60e67bf6e0b98e4ff807
anianuzhny <anianuzhny@scnsoft.com>
2021-08-04 12:00:18 +0300

    wiring up templates

--- aiohttp_study/polls/aiohttpdemo_polls/main.py
+++ aiohttp_study/polls/aiohttpdemo_polls/main.py
@@ -1,12 +1,16 @@
 # aiohttpdemo_polls/main.py
 from routes import setup_routes
 from aiohttp import web
-from settings import config
+import aiohttp_jinja2
+import jinja2
+from settings import config, BASE_DIR
 from db import init_pg, close_pg
 
 app = web.Application()
 setup_routes(app)
 app['config'] = config
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
 web.run_app(app)
--- aiohttp_study/polls/aiohttpdemo_polls/views.py
+++ aiohttp_study/polls/aiohttpdemo_polls/views.py
@@ -2,9 +2,14 @@
 from aiohttp import web
 import db
 
+import aiohttp_jinja2
+
+@aiohttp_jinja2.template('index.html')
 async def index(request):
     async with request.app['db'].acquire() as conn:
         cursor = await conn.execute(db.question.select())
         records = await cursor.fetchall()
         questions = [dict(q) for q in records]
-        return web.Response(text=str(questions))
+        print(questions)
+        # return web.Response(text=str(questions))
+        return {"questions": questions}
